9f720915081a42e7ab6aa57e315fa1296d74b627
ttab303 <ttab303@gmail.com>
2016-04-04 15:25:58 +0100

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,18 @@
+
+data <- read.csv("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";", nrows=2075259, check.names=FALSE, stringsAsFactors=FALSE, comment.char="", quote="\"")
+
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+util  <- subset(data, subset=(Date >= "2007/02/01" & Date <= "2007/02/02"))
+rm(data)
+
+with(util,plot(Sub_metering_1 ~ Datetime, ylab="Energy sub metering", xlab="", type="l"))
+
+with(util,lines(Sub_metering_2~Datetime,col="Red"))
+
+with(util,lines(Sub_metering_3 ~ Datetime, col="Blue"))
+legend("topright", legend=c("Sub_metering_1   ", "Sub_metering_2   ", "Sub_metering_3   "), col=c("Black", "Red", "Blue"), lty=1, lwd=1, cex = 0.6)
+
+dev.copy(png, file="plot3.png", width=480, height=480)
+dev.off()
